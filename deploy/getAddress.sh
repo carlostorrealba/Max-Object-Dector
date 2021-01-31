2a5fc6846690c8a62581aaa70db183c03e6764ca
jaumemir <jaume.miralles@es.ibm.com>
2021-01-31 17:03:28 +0100

    Create getAddress.sh

---
+++ deploy/getAddress.sh
@@ -0,0 +1,3 @@
+IP=`ibmcloud ks workers --cluster mycluster-free | grep [1-9] | awk '{print $2}'`
+PORT=`kubectl describe service max-object-detector -n prod | grep NodePort |grep TCP |awk '{print $3}' | cut -f1 -d'/'`
+echo "Visit APP in: http://${IP}/${PORT}"
